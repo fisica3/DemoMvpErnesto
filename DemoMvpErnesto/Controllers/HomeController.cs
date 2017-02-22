d3978f8b329e641887c39a8743daa365592affa0
Ernesto Cárdenas <ernesto.cardenas@clarabridge.com>
2017-02-22 14:28:06 -0500

    Agregar archivos de proyecto.

---
+++ DemoMvpErnesto/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoMvpErnesto.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Esta es una prueba de concepto con .Net Core.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Pagina de contactos en Construcción.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
